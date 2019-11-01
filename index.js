bb69c1c3b6c11791049218c5a5587eb6f17da483
Sokos6 <will.sokolowski@gmail.com>
2019-11-01 15:39:06 -0500

    update html and js

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-'use strict';
+"use strict";
 
 function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+  fetch("https://dog.ceo/api/breeds/image/random")
     .then(response => response.json())
-    .then(responseJson =>
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert("Something went wrong. Try again later."));
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
-  $('.results-img').replaceWith(
+  $(".results-img").replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
-  )
+  );
   //display the results section
-  $('.results').removeClass('hidden');
+  $(".results").removeClass("hidden");
 }
 
 function watchForm() {
-  $('form').submit(event => {
+  $("form").submit(event => {
     event.preventDefault();
     getDogImage();
   });
 }
 
 $(function() {
-  console.log('App loaded! Waiting for submit!');
+  console.log("App loaded! Waiting for submit!");
   watchForm();
 });
